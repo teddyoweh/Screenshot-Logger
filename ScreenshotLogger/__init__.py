539d595a3893553763295ab2f12bf00b2449b9fb
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2021-12-14 01:49:29 -1100

    Create __init__.py

---
+++ ScreenshotLogger/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding=utf-8 -*-
+# Name: teddy oweh
+# Email: teddyoweh@gmail.com
+# Message: Feel Free To Contact Me on Enquires or Question, il Reply :)
+from ScreenshotLogger.screenshotLogger import screenshotLogger
